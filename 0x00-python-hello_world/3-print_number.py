6241c4a7e7c3483b7729d7f0e807a56063b20bc7
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-15 17:33:45 +0000

    removed a new line in print statement of tasks #3, #4, #5 && added a starting slice of first_3 In task #7

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print("{:d} {:s}\n".format(number, "Battery street"))
+print("{:d} {:s}".format(number, "Battery street"))
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-print("{:s} {:0.2f}\n".format("Float:", number))
+print("{:s} {:0.2f}".format("Float:", number))
