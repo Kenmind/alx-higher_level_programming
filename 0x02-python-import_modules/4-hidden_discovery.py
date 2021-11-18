ff79c369adc15ba258c9304efed15b0f687ebb4c
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-18 17:54:34 +0000

    removed blank lines and checked on some requirements

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
     funcs = dir(hedden_4)
     for i in funcs:
-        if i[:2] != "__":
+        if not i.startswith("__"):
 
-            print(i)
+            print("{:s}".format(i))
