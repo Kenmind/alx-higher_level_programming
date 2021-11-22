0f84e77db4ce2a76d7068b7284ca3c21ba279f4d
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-22 08:06:28 +0000

    completed task #4wq

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(len(my_list)):
-        i = my_list[idx]
-        idx = my_list.index(i)
     if idx < 0 or idx > (len(my_list) - 1):
         return None
     return(my_list[idx])
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list)):
+    if isinstance(my_list, list):
         my_list.reverse()
-        print("{}".format(my_list[i]))
-    return my_list
+        for i in my_list:
+            print("{:d}".format(i))
