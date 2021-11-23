51fc37a9b31fc722e2252cf4d678f3c533555f10
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-23 06:37:54 +0000

    specified my_list element in #10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     div = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             div.append(True)
-        elif i % 2 != 0:
+        elif my_list[i] % 2 != 0:
             div.append(False)
     return div
