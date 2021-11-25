20bf727d523647cca548c1df3fa05fe955b95490
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-25 07:47:06 +0000

    completed 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,11 +1,58 @@
-#include <Python.h>
-#include <bytesobject.h>
+#include "/usr/include/python3.4/Python.h"
+#include "/usr/include/python3.4/bytesobject.h"
 #include <object.h>
 #include <listobject.h>
+#include <stdio.h>
 
 #define GET PyList_GetItem
 #define SIZE Py_SIZE
 
+/**
+ * print_hexn - prints hex integer
+ * @str: string
+ * @n: int
+ */
+
+void print_hexn(const char *str, int n)
+{
+	int i = 0;
+
+	for (; i < n - 1; i++)
+		printf("%02x ", (unsigned char) str[i]);
+
+	printf("%02x", str[i]);
+}
+
+/**
+ * print_python_bytes - prints python bytes
+ * @p: PyObject
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *clone = (PyBytesObject *) p;
+	int calc_bytes, clone_size = 0;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(clone))
+	{
+		clone_size = PyBytes_Size(p);
+		calc_bytes = clone_size + 1;
+
+		if (calc_bytes >= 10)
+			calc_bytes = 10;
+
+		printf("  size: %d\n", clone_size);
+		printf("  trying string: %s\n", clone->ob_sval);
+		printf("  first %d bytes: ", calc_bytes);
+		print_hexn(clone->ob_sval, calc_bytes);
+		printf("\n");
+	}
+	else
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+	}
+}
 
 /**
  * print_python_list - prints info about python lists
@@ -31,12 +78,3 @@ void print_python_list(PyObject *p)
 	}
 }
 
-/**
- * print_python_bytes - Prints info about Python Bytes
- * @p: PyObject
- */
-
-void print_python_bytes(PyObject *p)
-{
-	
-}
