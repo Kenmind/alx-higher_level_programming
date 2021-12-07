5b37f3146f4197dd3cc4fef6ba09a6929234f108
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-07 14:39:43 +0000

    completed task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module contains class Square"""
+
+
+class Square:
+    """Instantiates a private instance attribute
+    with no type/value verification"""
+    def __init__(self, size):
+        """Initializes a square.
+        Arg:
+            size: size of the square
+        """
+        self.__size = size
