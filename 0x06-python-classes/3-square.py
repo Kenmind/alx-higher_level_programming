d6f85c818573b9ad4221119710e3a8b2f56d5753
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-07 15:27:07 +0000

    completed task #3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """has public instance area"""
+    def __init__(self, size=0):
+        """validates the value and type of size
+        Args:
+        size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the current square"""
+        return self.__size * self.__size
