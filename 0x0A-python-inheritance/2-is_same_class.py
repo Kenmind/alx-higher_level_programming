82c6cd2accb8d0fc8ad468bd0828f9aacd665f6e
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-21 07:11:56 +0000

    completed task #2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,6 +8,6 @@ def is_same_class(obj, a_class):
         obj: object
         a_class: class
     """
-    if isinstance(obj, a_class) is True:
+    if type(obj) == a_class:
         return True
     return False
