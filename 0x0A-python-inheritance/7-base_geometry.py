2ccf54a3dd4d47b2be4a672b9ce7e230199d9f9b
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-21 12:10:22 +0000

    corrected task #7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,6 +18,6 @@ class BaseGeometry:
             value: value
         """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
