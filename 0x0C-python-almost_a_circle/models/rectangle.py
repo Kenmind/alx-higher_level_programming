49452cf9b33002b459aa7219d55bb7f3c652d88b
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-28 13:30:35 +0000

    completed task #6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,3 +120,11 @@ class Rectangle(Base):
 
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Prints info about the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.__x,
+                                                       self.__y,
+                                                       self.__width,
+                                                       self.__height)
