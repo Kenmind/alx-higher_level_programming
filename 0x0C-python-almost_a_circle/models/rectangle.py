8fc4c244eacb8d0a8a7b71954ef497b2e5f14ed1
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-28 14:58:26 +0000

    checked indentation Error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,11 +130,12 @@ class Rectangle(Base):
 
     def __str__(self):
         """Prints info about the rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.__x,
-                                                       self.__y,
-                                                       self.__width,
-                                                       self.__height)
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
+                                                self.id,
+                                                self.__x,
+                                                self.__y,
+                                                self.__width,
+                                                self.__height)
 
     def update(self, *args, **kwargs):
         """updates values to attributes using *args"""
