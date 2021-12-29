a26a627e06b650487b85ce1feea3e61f932d9ab5
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-12-29 14:26:21 +0000

    completed task #16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,16 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string to file"""
+        js_str = '[]'
+        js_list = []
+        if list_objs is not None:
+            for i in list_objs:
+                js_list.append(i.to_dictionary())
+            if len(js_list) > 0:
+                js_str = Base.to_json_string(js_list)
+        with open(cls.__name__ + '.json', 'w') as n:
+            n.write(js_str)
