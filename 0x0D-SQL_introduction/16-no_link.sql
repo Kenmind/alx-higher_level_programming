7b03b782aa4747a30379149899e58fc1ddcb34b0
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-01 21:26:40 +0200

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table "second_table"
+	-- without listing rows without a name value
+		--in descending order
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
