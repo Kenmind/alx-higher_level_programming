7c7ea33f87493580120585342dcfb13dfcaf77a6
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-02 15:51:10 +0200

    corrected #16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,4 @@
 -- Lists all records of the table "second_table"
 	-- without listing rows without a name value
 		--in descending order
-SELECT score, name FROM second_table
-WHERE name != ""
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;
