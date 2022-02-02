cb644b59235219494fa5b02283f33421d78f8d42
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-02 22:56:30 +0200

    corrected #13

--- 0x0E-SQL-more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Creates a user "user_0d_1" with password "user_od_1_pwd"
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
