031b76faa0df80c1d7c29b0834f6ff8f53e3bede
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-28 11:49:33 +0200

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let myVar = 'C is fun';
+let i;
+for (i = 1; i < 4; i++) {
+  if (i === 2) { myVar = 'Python is cool'; }
+  if (i === 3) { myVar = 'Javascript is amazing'; }
+  console.log(myVar);
+}
