2e4107d20013762df94f1d85f091f2f0b84fff2c
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-28 15:47:10 +0200

    value args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.length;
-if (args === 2) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
