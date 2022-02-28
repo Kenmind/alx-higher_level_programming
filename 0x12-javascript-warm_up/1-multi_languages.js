6ef5d835a0a1d6d8299fec06b4f9fac976889547
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-28 15:44:11 +0200

    case of Javascript 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,7 @@ let myVar = 'C is fun';
 let i;
 for (i = 1; i < 4; i++) {
   if (i === 2) { myVar = 'Python is cool'; }
-  if (i === 3) { myVar = 'Javascript is amazing'; }
+  if (i === 3) { myVar = 'JavaScript is amazing'; }
   console.log(myVar);
 }
+
