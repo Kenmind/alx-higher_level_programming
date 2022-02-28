ce0230efdcb4b4b86432f726b0aea8002adff742
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-02-28 15:49:08 +0200

    case of Javascript Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < 3; i++) {
   console.log(arr[i]);
