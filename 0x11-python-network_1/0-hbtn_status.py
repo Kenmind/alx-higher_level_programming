9c6448da34dc0528b314359737cd603e969bdacb
Kenmind <kennedykalaluka04@gmail.com>
2022-03-18 17:27:11 +0200

    Corrected 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,11 @@
 
 import urllib.request
 
-
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url_o:
-    read_url = url_o.read()
-    print(" - {}".format(read_url))
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as url_o:
+        read_url = url_o.read()
+        print("Body response:")
+        print("\t- type:", type(read_url))
+        print("\t- content:", read_url)
+        print("\t- utf8 content:", read_url.decode("utf-8"))
