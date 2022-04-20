35496e467869363e0e40d6a0285a4646f3551a07
Kenmind <kennedykalaluka04@gmail.com>
2022-04-20 17:25:56 +0200

    Star wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
-let url = 'https://swapi-api.hbtn.io/api/films/?format=json'
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 $.get(url)
   .done(function (data) {
-    for (let i = 0; i < data['count']; i++) {
-      let movie = '<li>' + data['results'][i]['title'] + '</li>';
+    for (let i = 0; i < data.count; i++) {
+      const movie = '<li>' + data.results[i].title + '</li>';
       $('#list_movies').append(movie);
     }
   });
